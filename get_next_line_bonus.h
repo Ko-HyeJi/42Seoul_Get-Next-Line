b2433c205228896242f330363dceb8685e734d58
Ko-HyeJi <88470545+Ko-HyeJi@users.noreply.github.com>
2022-06-08 13:50:39 +0900

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 11:09:38 by hyko              #+#    #+#             */
+/*   Updated: 2022/02/21 19:03:21 by hyko             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef OPEN_MAX
+
+#  define OPEN_MAX 10240
+
+# endif
+
+char	*get_next_line(int fd);
+char	*gnl_expand_backup(char *backup, int fd);
+char	*gnl_cut_backup(char *backup, char *return_line);
+char	*gnl_read_line(int fd);
+int		gnl_find_newline(char *read_line);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const	*s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
